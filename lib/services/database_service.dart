2f2f6340f10262e2b687ea9fa746ce235cfaf4ab
tsutlovic <tsutlovic@gmail.com>
2025-05-28 12:39:00 -0400

    setup drift

---
+++ lib/services/database_service.dart
@@ -0,0 +1,26 @@
+import 'package:drift/drift.dart';
+import 'package:drift_flutter/drift_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+part 'database_service.g.dart';
+
+@DriftDatabase(tables: [])
+class AppDatabase extends _$AppDatabase {
+  @override
+  int get schemaVersion => 1;
+
+  AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
+
+  static QueryExecutor _openConnection() {
+    return driftDatabase(
+      name: 'my_database',
+      native: const DriftNativeOptions(
+        databaseDirectory: getApplicationSupportDirectory,
+      ),
+      web: DriftWebOptions(
+        sqlite3Wasm: Uri.parse('sqlite3.wasm'),
+        driftWorker: Uri.parse('drift_worker.js'),
+      ),
+    );
+  }
+}
